77a4c70358087ea471f9a55cb5286661b9b14746
Elton Passos <eltonps19@gmail.com>
2026-07-02 17:10:11 -0300

    idexs com links associativos para cada arquivo site.php

---
+++ models/site.php
@@ -0,0 +1,7 @@
+<?php 
+require "connect.php";
+
+echo "esta na parte do controller/site.php";
+echo '<a href="../controller/site.php">ir a controller </a>';
+echo '<a href="../views/site.php">ir a views </a>';
+?>
---
+++ views/site.php
@@ -0,0 +1,7 @@
+<?php 
+require "connect.php";
+
+echo "esta na parte do controller/site.php";
+echo '<a href="../models/site.php">ir a models </a>';
+echo '<a href="../controller/site.php">ir a controller </a>';
+?>
